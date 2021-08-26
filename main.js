02f71371355b8c4a8f5f2a1c28d81b36c559b6ec
ayhnkbk <68517296+ayhnkbk@users.noreply.github.com>
2021-08-26 18:29:51 +0300

    a

---
+++ main.js
@@ -0,0 +1,6 @@
+function changetext(){
+    document.getElementById("text").innerHTML = "ok go now"
+};
+function defaulttext(){
+    document.getElementById("text").innerHTML = "I'll put something here soon."
+};
